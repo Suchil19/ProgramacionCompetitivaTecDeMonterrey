fbaae5dbae30eb7c8fed784f9877ebc98fcb2abd
suchil19 <jlsuchil@gmail.com>
2025-01-19 22:27:52 -0600

    07

---
+++ Sesion-01/ejercicios/ejercicios-basicos.py
@@ -0,0 +1,26 @@
+# Hola Mundo
+print("Hola Mundo")
+
+# Creación de variables
+a = 10
+b = 20
+suma = a + b
+print(suma)
+
+#Ejemplo de concatenación
+a = 10
+b = 20
+suma = a + b
+print("este es tu resultadi", suma)
+
+#Tipos de datos - int
+a = int(input("escribe tu primer valor"))
+b = int(input("escribe tu segundo valor"))
+suma = a + b
+print("el resultado es ... ", suma)
+
+#Tipos de datos - float
+a = float(input("escribe tu primer valor"))
+b = float(input("escribe tu segundo valor"))
+div = a / b
+print("el resultado es ... ", div)
